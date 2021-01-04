58428494a886336aabc4df5be58ef21844a62133
laurent-offi <laurent.courcelle.lh@gmail.com>
2021-01-04 15:11:19 +0100

    Exercice 1 js2 fini

---
+++ script/result.js
@@ -0,0 +1,3 @@
+let image = document.getElementById("image1");
+image.onmouseover = function() {  image.src = "images/image1_2.jpg"; }
+image.onmouseout = function() {  image.src = "images/image1.jpg"; }
